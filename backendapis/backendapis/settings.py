7f7e95d34fbc50396084b06401b68357dfccb5e9
prajvalchavan99 <prajvalchavan99@gmail.com>
2023-10-16 22:55:49 +0530

    Vercel setup

--- backendapis/backendapis/settings.py
+++ backendapis/backendapis/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-tb=5ra7ypcd(^h&lnbbqvo9ry%&ezlfn^)f!xg3e#tj1%@_k9%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
